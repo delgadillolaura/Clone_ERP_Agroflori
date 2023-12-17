aa192009ec2da306ff4205449e749f9a93e8989d
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-12-17 19:27:29 -0400

    develop Modified files for deploying the application on Azure.

--- erpAgroflori/erpAgroflori/wsgi.py
+++ erpAgroflori/erpAgroflori/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erpAgroflori.settings')
+settings_module = 'erpAgroflori.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'erpAgroflori.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
